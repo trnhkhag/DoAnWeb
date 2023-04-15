ae3bd087539e9ae304f4e781e9c28121e10fdeb9
BigGok <ndtai0912@gmail.com>
2023-04-15 19:08:03 +0700

    up

--- logout.php
+++ logout.php
@@ -1,7 +1,6 @@
-<?php 
+<?php
 session_start();
-
-session_unset();
-session_destroy();
-
-header("Location: index.php");
+unset($_SESSION["MaNguoiDung"]);
+unset($_SESSION["TenDangNhap"]);
+header("Location:index.php");
+?>
